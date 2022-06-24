cfa3e38b767c820426740361595d1274057b5871
Kris Paschal <krispaschal@gmail.com>
2022-06-24 16:58:57 -0500

    added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import User from './pages/User'
+import Users from './pages/Users'
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/users' component={Users} />
+          <Route path='/user' component={User} />
+        </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
