d169d0326f9ff715a2bf89d9782837a28c121836
Kris Paschal <krispaschal@gmail.com>
2022-06-27 15:39:04 -0500

    added product fix

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function Products() {
+export default function Users() {
+  const [products, SetUsers] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://cardgametestserver.azurewebsites.net/product`).then((res) => {
+      const responseProducts = res.data
+      SetUsers(responseProducts)
+    })
+  }, [])
+
+
   return (
-    <div>
-      Products
-    </div>
+    <>
+       <h1>Users List</h1>
+       {users &&
+        users.map((user) => {
+          const { userId, username, password, email } = user
+          return (
+            <div key={userId}>
+              <h1>{username}</h1>
+              <h3>{password}</h3>
+              <h5>{email}</h5>
+            </div>
+          )})}
+    </>
   )
 }
