361a58adf51e332279cf676c0df4fd3920d15626
Kris Paschal <krispaschal@gmail.com>
2022-06-27 15:43:59 -0500

    product list fix again

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export default function Users() {
-  const [products, SetUsers] = useState([])
+export default function Products() {
+  const [products, SetProducts] = useState([])
 
   useEffect(() => {
     axios.get(`https://cardgametestserver.azurewebsites.net/product`).then((res) => {
       const responseProducts = res.data
-      SetUsers(responseProducts)
+      SetProducts(responseProducts)
     })
   }, [])
 
 
   return (
     <>
-       <h1>Users List</h1>
-       {users &&
-        users.map((user) => {
-          const { userId, username, password, email } = user
+       <h1>Product List</h1>
+       {products &&
+        products.map((product) => {
+          const { productId, productName, productDescription, productPrice } = product
           return (
-            <div key={userId}>
-              <h1>{username}</h1>
-              <h3>{password}</h3>
-              <h5>{email}</h5>
+            <div key={productId}>
+              <h1>{productName}</h1>
+              <h3>{productDescription}</h3>
+              <h5>{productPrice}</h5>
             </div>
           )})}
     </>
